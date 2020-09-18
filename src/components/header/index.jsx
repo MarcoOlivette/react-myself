b06e9ccfac0a22518f0009b2faf899777e9e7c5c
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-18 11:55:04 -0300

    create connection with axios[

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import "./styles.css"
 
-const Header = () => <header>iae</header>
+const Header = () => <header className="main-header">Site top</header>
 
 export default Header;
---
+++ src/pages/main/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Main = () => <h1> teste </h1>;
+export default Main;
