8d88f663c1caf0af65ad847147ffa442c7b2e3e2
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-20 11:55:40 -0300

    try to get a product

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Main from './pages/main'
+import Product from './pages/main'
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact paht="/" component={Main}></Route>
+            <Route paht="/products/:id" component={Product}></Route>
         </Switch>
     </BrowserRouter>
 )
