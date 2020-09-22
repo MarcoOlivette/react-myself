583e798259f69aca32353c3f276fd2b37525f998
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-21 21:31:42 -0300

    refactory with hooks

--- version1/src/pages/main/index.jsx
+++ version1/src/pages/main/index.jsx
@@ -1,36 +1,27 @@
-import React, { Component } from "react";
-import Api from '../../services/api'
-import {Link}  from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import Api from "../../services/api";
+import { Link } from "react-router-dom";
 
-export default class Main extends Component{
-    state = {
-        products : []
-    }
+export default function Main() {
+  const [products, setProduct] = useState([]);
 
-    componentDidMount(){
-        this.listProducts();
-    }
+  useEffect(() => {
+    listProducts();
+  }, []);
 
-    listProducts = async () => {
-        const response = await Api.get(`/products`);
-        this.setState({
-            products: response.data.docs
-        })
-    }
-
-    render () {
-        const {products} = this.state;
-        return (
-            <div className="product-list">
-                {
-                    products.map(product => (
-                        <div key={product._id}>
-                            <h1>{product.title}</h1>
-                            <Link to={`/products/${product._id}`}> Acessar</Link>
-                        </div>
-                    ))
-                }
-            </div>
-        )
-    }
+  const listProducts = async () => {
+    const response = await Api.get(`/products`);
+    setProduct(response.data.docs);
+  };
+  
+  return (
+      <div className="product-list">
+        {products.map((product) => (
+          <div key={product._id}>
+            <h1>{product.title}</h1>
+            <Link to={`/products/${product._id}`}> Acessar</Link>
+          </div>
+        ))}
+      </div>
+    );
 }
--- version1/src/pages/product/index.jsx
+++ version1/src/pages/product/index.jsx
@@ -1,31 +1,30 @@
-import React, { Component } from "react";
+import React, { useEffect, useState} from "react";
 import Api from "../../services/api";
+import './styles.css'
 
-export default class Product extends Component {
-    state = {
-        product: [],
-      };
-    
-      async componentDidMount() {
-        const { id } = this.props.match.params;
-        const response = await Api.get(`/products/${id}`);
-        this.setState({
-          product: response.data,
-        });
-      }
-    
-      render() {
-        const { product } = this.state;
-        return (
-          <h1>
-            <div className="product-info">
-                <h1>{product.title}</h1>
-                <p>{product.description}</p>
-                <p>
-                    URL: <a href={product.url} target="_.blanket">{product.url}</a>
-                </p>
-            </div>
-          </h1>
-        );
-      }
-}
+export default function Product(){
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    getProduct();
+  })
+
+  const getProduct = async () => {
+    // const { id } = this.props.match.params;
+    const response = await Api.get(`/products/5b6b31eb31762700049b33df`);
+    setProduct(response.data);
+  }
+
+  return (
+    <h1>
+      <div className="product-info">
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <p>
+              URL: <a href={product.url} target="_.blanket">{product.url}</a>
+          </p>
+      </div>
+    </h1>
+  );
+} 
